a31d80d5795ad67e68fa12e11b5381f9d37f8bd4
Ambuj Kumar <sendambuj@gmail.com>
2014-05-23 19:47:35 -0400

    Update MatrixCol.py

--- MatrixCol.py
+++ MatrixCol.py
@@ -1,6 +1,6 @@
 ################################################################################################################
 #                                                                                                              #
-# Copyright (C) {2014}  {Ambuj Kumar, Kimball-Brain lab group, Biology Department, University of Florida}      #
+# Copyright (C) {2014}  {Ambuj Kumar, Kimball-Braun lab group, Biology Department, University of Florida}      #
 #                                                                                                              #
 # This program is free software: you can redistribute it and/or modify                                         #
 # it under the terms of the GNU General Public License as published by                                         #
